5160e47d73b3f8f967643d5a9f9024e43e258e09
graff <graffaya@gmail.com>
2025-03-27 18:10:01 +0200

    remove favorite button

--- app/src/main/java/com/graff/tester/gallery.java
+++ app/src/main/java/com/graff/tester/gallery.java
@@ -9,13 +9,18 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.Arrays;
 import java.util.List;
 
-public class gallery extends AppCompatActivity { // שיניתי את השם ל-GalleryActivity לפי מוסכמות השמות
+public class gallery extends AppCompatActivity {
     private RecyclerView recyclerView;
     private GalleryAdapter adapter;
     private Button btnBack;
     private List<Integer> imageList = Arrays.asList(
             R.drawable.shirt1,
             R.drawable.shirt2,
+            R.drawable.shirt3,
+            R.drawable.shirt4,
+            R.drawable.shirt5,
+            R.drawable.pants,
+            R.drawable.pants2,
             R.drawable.pants3,
             R.drawable.pants4,
             R.drawable.pants5,
@@ -30,7 +35,7 @@ public class gallery extends AppCompatActivity { // שיניתי את השם ל-
         btnBack.setOnClickListener(v -> finish());
         recyclerView = findViewById(R.id.recyclerView_gallery);
 
-        GridLayoutManager layoutManager = new GridLayoutManager(this, 3); // 3 עמודות
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 3);
         recyclerView.setLayoutManager(layoutManager);
 
         adapter = new GalleryAdapter(this, imageList);
